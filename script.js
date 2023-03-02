75a6fc8388f76b2d4042bfe8fbab08ceb7f56c3e
Dabi-ayu <dabiayu8@gmail.com>
2023-03-01 16:21:34 -0800

    Todolist

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector(".add").onclick = function () {
+    if (document.querySelector(".new-task-input").value === "") {
+        alert("please input a task");
+
+    } else {
+        document.querySelector(".todo-section").innerHTML += ` <section class = "flex-delete"><div class="task"><span id = "taskname">
+        ${document.querySelector(".new-task-input").value}</span>
+        </div>  <button class = "delete">
+        <i class = "fa fa-trash"></i>
+        <button>
+        </section>`;
+
+        let currentTask = document.querySelectorAll(".delete");
+        for (let i = 0; i < currentTask.length; i++) {
+            currentTask[i].onclick = function () {
+                this.parentNode.remove();
+            }
+
+        }
+    }
+}//
